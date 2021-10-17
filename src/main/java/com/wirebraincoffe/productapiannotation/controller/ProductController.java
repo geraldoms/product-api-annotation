28991bf1c812cedd90d89bca208e91ab53711cd3
Geraldo Silva <geraldoms007@gmail.com>
2021-10-17 19:01:13 -0400

    Product API - Get method

--- src/main/java/com/wirebraincoffe/productapiannotation/controller/ProductController.java
+++ src/main/java/com/wirebraincoffe/productapiannotation/controller/ProductController.java
@@ -1,9 +1,24 @@
 package com.wirebraincoffe.productapiannotation.controller;
 
+import com.wirebraincoffe.productapiannotation.model.Product;
+import com.wirebraincoffe.productapiannotation.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    private ProductRepository repository;
+
+    public ProductController(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Flux<Product> getAllProducts() {
+        return repository.findAll();
+    }
 }
